9a9304931dafe641a58536b1a0dcfe5a862511d0
oyinmienye <chcisegbele@gmail.com>
2023-12-07 07:01:49 -0800

    added new email_template.sql

---
+++ email_template.sql
@@ -0,0 +1,1 @@
+create table email_template( id INTEGER PRIMARY KEY  ,type_sent VARCHAR NOT NULL,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP , deleted_at TIMESTAMP , update_at TIMESTAMP, content TEXT NOT NULL );
